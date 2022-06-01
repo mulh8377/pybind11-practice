fddd0eefaaaaa359e71ec3ab06dd657a62e56bc3
mulh8377 <vontossman@gmail.com>
2022-05-31 22:47:12 -0500

    update

--- src/linkedlist.h
+++ src/linkedlist.h
@@ -5,45 +5,58 @@
 #ifndef CMAKE_EXAMPLE_LINKEDLIST_H
 #define CMAKE_EXAMPLE_LINKEDLIST_H
 
+#include <pybind11/pybind11.h>
+#include <fplus/fplus.hpp>
 #include <memory>
 
-namespace container {
+#include <sstream>
+#include <algorithm>
 
-    class LinkedList {
-        struct Node {
-            int data;
-            std::unique_ptr<Node> next;
-        };
-    private:
-        std::unique_ptr<Node> container;
-        uint64_t memory;
-    public:
-        LinkedList() = default;
-        ~LinkedList() = default;
 
-        void push(const int data) {
-            container = std::unique_ptr<Node>(new Node{data, std::move(container)});
-            memory++;
-        }
-
-        const int pop() {
-            const int p = container->data;
-            container = std::move(container->next);
-            memory--;
-            return p;
-        }
-    };
+namespace detail {
 
     namespace py = pybind11;
 
-    static void bind_class_LinkedList(py::module_ &m) {
-
-        py::class_<LinkedList>(m, "LinkedList")
-                .def(py::init<>())
-                .def("push", &LinkedList::push)
-                .def("pop", &LinkedList::pop);
+    template<typename Vector, typename Class_>
+    void list_modifiers(Class_ &c1) {
+        c1.def("clear", [](Vector &v) { v.clear(); });
+    }
+    template<typename Vector, typename Class_>
+    void list_accessors(Class_ &c1) {
+        c1.def("get", [](Vector &v) { return fplus::numbers(1, 30); } );
     }
 
-} // container
 
+namespace container {
+
+    template <typename Vector, typename holder_type = std::unique_ptr<Vector>, typename... Args>
+    py::class_<Vector, holder_type> bind_list(pybind11::handle scope, std::string const &name, Args&&...args) {
+        using Class_ = py::class_<Vector, holder_type>;
+
+        using vtype = typename Vector::value_type;
+        auto *vtype_info = pybind11::detail::get_type_info(typeid(vtype));
+        bool local = !vtype_info || vtype_info->module_local;
+
+
+        Class_ c1(scope, name.c_str(), pybind11::module_local(local), std::forward<Args>(args)...);
+
+        c1.def(py::init<>());
+
+        detail::list_modifiers<Vector, Class_>(c1);
+        detail::list_accessors<Vector, Class_>(c1);
+
+        c1.def("__len__", &Vector::size);
+        c1.def("capacity", &Vector::capacity);
+        c1.def("max_size",      &Vector::max_size,      "returns the maximum possible number of elements");
+
+        //Class_ c1(name.c_str());
+
+
+
+        return c1;
+
+    }
+
+    } // container
+}
 #endif //CMAKE_EXAMPLE_LINKEDLIST_H
--- src/main.h
+++ src/main.h
@@ -6,17 +6,20 @@
 #define CMAKE_EXAMPLE_HAND_H
 
 #include <pybind11/pybind11.h>
+#include <pybind11/stl_bind.h>
 
 #include "linkedlist.h"
 #include "map.h"
 #include "tree.h"
+
+PYBIND11_MAKE_OPAQUE(std::vector<int>);
 
 namespace container {
 
     namespace py = pybind11;
 
     void bind_linkedlist(py::module_ &m) {
-        bind_class_LinkedList(m);
+        detail::container::bind_list<std::vector<int>>(m, "LinkedList");
     }
     void bind_tree(py::module_& m) {
         bind_class_tree(m);
